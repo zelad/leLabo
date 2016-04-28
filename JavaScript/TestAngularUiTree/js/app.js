9717b601d116d15d31baf640a936df27e91f0cf1
zelad <rodrigue.beck@gmail.com>
2016-04-28 17:54:17 +0200

    Test de Ui-Tree avec ngRoute Merdouille
    	il semblerait que cela ne fonctionne pas en local il faut un serveur...
    	Fonctionnouille sous FireFox, Chrome aurrait fais des mise à jour de sécurité!

--- JavaScript/TestAngularUiTree/js/app.js
+++ JavaScript/TestAngularUiTree/js/app.js
@@ -1,6 +1,18 @@
 /**
- * 
+ * utilisé pour le test de UI-Tree
  */
-//var WSTestApp = angular.module('WSocketTestApp', [
-//'wSockService',
-//]);
+(function() {
+	'use strict';
+
+	angular.module('demoApp', [ 'ui.tree', 'ngRoute', 'ui.bootstrap' ]).config(
+			[ '$routeProvider', '$compileProvider',
+					function($routeProvider, $compileProvider) {
+						$routeProvider.when('/first-try', {
+							controller : 'FirstTryCtrl',
+							templateUrl : 'views/first-try.html'
+						}).otherwise({
+							redirectTo : '/first-try'
+						});
+					}
+			]);
+})();
