376fc0c7971cfbf4242b91a4279cf7286851a07a
Emrik <rodrigue.beck@gmail.com>
2016-04-29 18:26:27 +0200

    Test avec les exemple du site de angualr-ui-tree
    il reste un problème avec des images...

--- JavaScript/TestAngularUiTree/js/controller.js
+++ JavaScript/TestAngularUiTree/js/controller.js
@@ -6,6 +6,36 @@
 angular.module('demoApp', ['ui.bootstrap']).controller("FirstTryCtrl",
 		function($scope, $rootScope, $timeout) {
 
+    $scope.remove = function (scope) {
+        scope.remove();
+      };
+
+      $scope.toggle = function (scope) {
+        scope.toggle();
+      };
+
+      $scope.moveLastToTheBeginning = function () {
+        var a = $scope.data.pop();
+        $scope.data.splice(0, 0, a);
+      };
+
+      $scope.newSubItem = function (scope) {
+        var nodeData = scope.$modelValue;
+        nodeData.nodes.push({
+          id: nodeData.id * 10 + nodeData.nodes.length,
+          title: nodeData.title + '.' + (nodeData.nodes.length + 1),
+          nodes: []
+        });
+      };
+
+      $scope.collapseAll = function () {
+        $scope.$broadcast('angular-ui-tree:collapse-all');
+      };
+
+      $scope.expandAll = function () {
+        $scope.$broadcast('angular-ui-tree:expand-all');
+      };
+	
 			$scope.data = [ {
 				'id' : 1,
 				'title' : 'node1',
