c290e74fd3f74f1ebfa9029f7afc9a3766f6529c
Emrik <rodrigue.beck@gmail.com>
2016-06-12 17:47:44 +0200

    Fin de session reception d'une arboressence ok!

--- JavaScript/TestAngularBootstrapNavTree/js/controller.js
+++ JavaScript/TestAngularBootstrapNavTree/js/controller.js
@@ -5,8 +5,13 @@
 var app = angular.module('uneApp', ['angularBootstrapNavTree']);
 
 app.controller("uneFonctionCtrl", function ($scope, $rootScope,$timeout, WebSocketService) {
-	var charge;
+	
+	$rootScope.ws = new SocketManager.SocketManager($scope,$rootScope);
 
+	txLogin = function() {
+		WebSocketService.sendObject("login",null);
+	}
+	
 $scope.data = [{
 		label: 'Languages',
 		children: ['Jade','Less','Coffeescript'],
@@ -48,5 +53,29 @@ $scope.data = [{
 	
 });
 
-
+var SocketManager = {
+		ws:{},
+		
+		SocketManager: function (scope,rootScope){
+	        this.ws = new WebSocket("ws://localhost:9999/");
+//	        this.ws = new WebSocket("ws://localhost:80/");
+//	        this.ws = new WebSocket("ws://172.28.50.187/");//ne fonctionne pas chez Ali le: 28/1/2016
+	        
+	        this.ws.onopen = function(){
+	        	console.log("Socket has been opened!");
+	        	txLogin();
+	        };
+	        
+	    	this.ws.onmessage = function(message) {
+	//console.log(message);
+	    	    var messageObj = JSON.parse(message.data);
+	    	    
+	  console.log("Received data from websocket: ", messageObj);
+	    	    rxTools.rxRouting(scope,rootScope,messageObj);
+	    	};
+	        
+	        return this.ws;
+	    }
+		
+};
 
